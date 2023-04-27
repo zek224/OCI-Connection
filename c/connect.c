31ea32311c186a5784196efc93573cad34b42989
zek224 <zek224@lehigh.edu>
2023-04-26 23:17:36 -0400

    column name func

--- c/connect.c
+++ c/connect.c
@@ -12,6 +12,7 @@ void print_oci_error(OCIError *errhp);
 int read_key_file(char *username, char *password, char *wallet_pw);
 void clean_up(OCISession *usrhp, OCISvcCtx *svchp, OCIServer *srvhp, OCIError *errhp, OCIEnv *envhp);
 int set_environment();
+void get_column_names(OCIEnv* envhp, OCISvcCtx* svchp, OCIError* errhp, const char* table_name);
 
 int main() {
     // Initialize OCI environment
@@ -94,7 +95,6 @@ int main() {
         return 1;
     }
 
-
     printf("Connected to Oracle Database.\n");
     printf("Select an option: \n");
     printf("1. Enter SQL statement.\n");
@@ -113,7 +113,7 @@ int main() {
     // if option is 1, take user input for sql statement
     if(option == 1) {
         printf("Enter SQL statement (dont add ';'): ");
-        // get user input with spaces into sql_statement
+        // get user input
         scanf(" %[^\n]s", sql_statement);
         sql_statement[sizeof(sql_statement) - 1] = '\0';  // Add null terminator
         printf("SQL statement: %s\n", sql_statement);
@@ -237,3 +237,79 @@ int set_environment() {
     }
     return 0;
 }
+
+
+// SELECT column_name FROM user_tab_columns WHERE table_name = test
+// void get_column_names(OCIEnv* envhp, OCISvcCtx* svchp, OCIError* errhp, const char* table_name) {
+//     OCIStmt* stmthp;
+//     sword status;
+
+//     // Prepare the SQL statement to retrieve column names
+//     const char* sql_statement = "SELECT column_name FROM user_tab_columns WHERE table_name = :table_name";
+//     status = OCIHandleAlloc((dvoid *)envhp, (dvoid **)&stmthp, OCI_HTYPE_STMT, (size_t)0, (dvoid **)0);
+//     if (status != OCI_SUCCESS) {
+//         printf("OCIHandleAlloc failed.\n");
+//         return;
+//     }
+//     status = OCIStmtPrepare(stmthp, errhp, (text *)sql_statement, (ub4)strlen(sql_statement), (ub4)OCI_NTV_SYNTAX, (ub4)OCI_DEFAULT);
+//     if (status != OCI_SUCCESS) {
+//         printf("OCIStmtPrepare failed.\n");
+//         return;
+//     }
+
+//     // Bind the table name to the statement
+//     OCIBind* bindp;
+//     const int table_name_length = strlen(table_name);
+//     status = OCIBindByName(stmthp, &bindp, errhp, (text *)":table_name", -1, (void *)table_name, table_name_length, SQLT_STR, 0, 0, 0, 0, 0, OCI_DEFAULT);
+//     if (status != OCI_SUCCESS) {
+//         printf("OCIBindByName failed.\n");
+//         return;
+//     }
+
+//     // Execute the statement
+//     status = OCIStmtExecute(svchp, stmthp, errhp, (ub4)1, (ub4)0, (OCISnapshot *)NULL, (OCISnapshot *)NULL, (ub4)OCI_DEFAULT);
+//     if (status != OCI_SUCCESS) {
+//         printf("OCIStmtExecute failed.\n");
+//         print_oci_error(errhp);
+//         return;
+//     }
+
+//     // Fetch and print column names
+//     OCIParam* paramhp;
+//     ub2 num_cols;
+//     ub2 i;
+//     status = OCIStmtFetch(stmthp, errhp, (ub4)1, (ub4)OCI_FETCH_NEXT, (ub4)OCI_DEFAULT);
+//     if (status != OCI_SUCCESS) {
+//         printf("OCIStmtFetch failed.\n");
+//         return;
+//     }
+//     status = OCIAttrGet((dvoid *)stmthp, (ub4)OCI_HTYPE_STMT, (dvoid *)&paramhp, (ub4 *)0, (ub4)OCI_ATTR_PARAM, errhp);
+//     if (status != OCI_SUCCESS) {
+//         printf("OCIAttrGet failed.\n");
+//         return;
+//     }
+//     status = OCIAttrGet((dvoid *)paramhp, (ub4)OCI_DTYPE_PARAM, (dvoid *)&num_cols, (ub4 *)0, (ub4)OCI_ATTR_NUM_PARAMS, errhp);
+//     if (status != OCI_SUCCESS) {
+//         printf("OCIAttrGet failed.\n");
+//         return;
+//     }
+
+//     printf("Column Names:\n");
+//     for (i = 1; i <= num_cols; i++) {
+//         text* col_name;
+//         ub4 col_name_len;
+//         status = OCIParamGet((dvoid *)paramhp, (ub4)OCI_DTYPE_PARAM, errhp, (dvoid **)&paramhp, (ub4)i);
+//         if (status != OCI_SUCCESS) {
+//             printf("OCIParamGet failed.\n");
+//             return;
+//         }
+//         status = OCIAttrGet((dvoid *)paramhp, (ub4)OCI_DTYPE_PARAM, (dvoid *)&col_name, (ub4 *)&col_name_len, (ub4)OCI_ATTR_NAME, errhp);
+//         if (status != OCI_SUCCESS) {
+//             printf("OCIAttrGet failed.\n");
+//             return;
+//         }
+//         printf("%.*s\n", col_name_len, col_name);
+//     }
+// }
+
+
